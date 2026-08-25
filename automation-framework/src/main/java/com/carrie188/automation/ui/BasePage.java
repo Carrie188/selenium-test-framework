7b9623252bd2d121dca42a6bee4ef4dc3b963009
Carrie188 <81711342+Carrie188@users.noreply.github.com>
2026-08-24 17:47:38 -0700

    feat: add shared BasePage

---
+++ automation-framework/src/main/java/com/carrie188/automation/ui/BasePage.java
@@ -0,0 +1,23 @@
+package com.carrie188.automation.ui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public abstract class BasePage {
+    protected final WebDriver driver;
+    private final WebDriverWait wait;
+
+    protected BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    protected WebElement waitUntilVisible(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+}
